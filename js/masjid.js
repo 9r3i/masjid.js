5937be6a9747a30bc722a54a0fd69828cb592061
9r3i <luthfie@y7mail.com>
2025-03-13 10:46:53 +0700

    Create masjid.js

---
+++ js/masjid.js
@@ -0,0 +1,65 @@
+
+
+
+/* browser app */
+if(typeof HELPER_BROWSER_APP==='undefined'){
+  const HELPER_BROWSER_APP=false;
+}
+
+/* initialize the app */
+const Masjid=new MasjidApp;
+Masjid.start();
+window.MasjidFront=Masjid;
+
+/* masjid app */
+;function MasjidApp(){
+this.helperVersion='1.3.8';
+this.AppURL='https://cdn.jsdelivr.net/npm/@9r3i/helper@'+this.helperVersion+'/app.js';
+const meta=document.querySelector('meta[name="masjid-host"]');
+this.srcURL=meta?meta.content:'';
+this.files=[
+  'js/finance.js',
+];
+/* load everything and start the app */
+this.start=async function(){
+  if(location.hostname!='127.0.0.1'
+      &&location.hostname!='localhost'&&!meta){
+    alert('Error: Failed to start the app!');
+    return;
+  }
+  let script=await this.loadScript(this.AppURL),
+  app=new App({
+    helperVersion:this.helperVersion,
+    helperProduction:true,
+    localHelper:false,
+    logo:this.srcURL+'images/icon.png',
+    css:this.srcURL+'css/masjid.css',
+    configFile:this.srcURL+'config.json',
+    appHost:this.srcURL,
+    autoStart:false,
+    test:function(){
+      alert('this is a test');
+    },
+  });
+  /* production */
+  app.production=true;
+  /* load files */
+  for(let file of this.files){
+    await this.loadScript(this.srcURL+file);
+  }
+  /* start the app */
+  let start=await app.start();
+  /* clone the app to main object */
+  this.app=app;
+};
+/* load scriot as string */
+this.loadScript=async function(url='',type='text/javascript'){
+  let el=document.createElement('script'),
+  text=await fetch(url).then(r=>r.text());
+  el.type=type;
+  el.textContent=text;
+  document.head.append(el);
+  return el;
+};
+};
+
